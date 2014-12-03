b8f19038f78b464e5a48d127fd78039d50c55a52
nicolay870@gmail.com <nicolay870@gmail.com@e894bab0-515d-e7b2-031d-1052601404cc>
2014-12-03 17:03:59 +0000

    нарисовал стрелку

--- gui/AzGraphicsSelectedItemArrow.cpp
+++ gui/AzGraphicsSelectedItemArrow.cpp
@@ -1,6 +1,18 @@
 #include "AzGraphicsSelectedItemArrow.h"
 #include <QDebug>
 
+QPolygon AzGraphicsSelectedItemArrow::arrowPolygon(){
+    QPolygon poligon;
+    poligon << QPoint(5,10)
+            << QPoint(-10,30)
+            << QPoint(0,30)
+            << QPoint(0,50)
+            << QPoint(10,50)
+            << QPoint(10,30)
+            << QPoint(20,30);
+    return poligon;
+}
+
 void AzGraphicsSelectedItemArrow::show(QPainter *painter, QGraphicsItem *item){
     QPointF posi = item->pos();
     QRectF bound = item->boundingRect();
@@ -8,8 +20,8 @@ void AzGraphicsSelectedItemArrow::show(QPainter *painter, QGraphicsItem *item){
     int y1 = posi.y();
     int x2 = x1 + bound.width();
     int y2 = y1 + bound.height();
-    painter->drawRect(x1+bound.width()/2,y2,10,20);
-    //QPolygonF
-    //painter->drawPolygon(,0);
+    //painter->drawRect(x1+bound.width()/2,y2,10,20);
+
+    painter->drawPolygon(arrowPolygon(),Qt::WindingFill);
     //qDebug() << ;
 }
