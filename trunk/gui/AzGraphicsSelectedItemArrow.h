fb981574c0d869428aa3c0326540eb9976d8c464
nicolay870@gmail.com <nicolay870@gmail.com@e894bab0-515d-e7b2-031d-1052601404cc>
2014-12-03 17:03:59 +0000

    нарисовал стрелку

--- trunk/gui/AzGraphicsSelectedItemArrow.h
+++ trunk/gui/AzGraphicsSelectedItemArrow.h
@@ -9,6 +9,7 @@ public:
     AzGraphicsSelectedItemArrow(){}
    ~AzGraphicsSelectedItemArrow(){}
    void show(QPainter* painter, QGraphicsItem* item);
+   QPolygon static arrowPolygon();
 private:
     AzGraphicsSelectedItemArrow(const AzGraphicsSelectedItemArrow&);
     AzGraphicsSelectedItemArrow& operator = (const AzGraphicsSelectedItemArrow&){}
