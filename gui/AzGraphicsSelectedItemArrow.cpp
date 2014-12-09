29c23f3b4d0e016f5af7b4e9b52d73e638d070e1
nicolay870@gmail.com <nicolay870@gmail.com@e894bab0-515d-e7b2-031d-1052601404cc>
2014-12-09 13:56:59 +0000

    добавлено перечисление СТОРОНІ СВЕТА

--- gui/AzGraphicsSelectedItemArrow.cpp
+++ gui/AzGraphicsSelectedItemArrow.cpp
@@ -1,27 +1,66 @@
 #include "AzGraphicsSelectedItemArrow.h"
 #include <QDebug>
+#include <QGraphicsScene>
+#include <QGraphicsItem>
+
+
+AzGraphicsSelectedItemArrow::AzGraphicsSelectedItemArrow(QGraphicsScene *scene):mScene(scene){}
+
 
 QPolygon AzGraphicsSelectedItemArrow::arrowPolygon(){
     QPolygon poligon;
-    poligon << QPoint(5,10)
-            << QPoint(-10,30)
-            << QPoint(0,30)
-            << QPoint(0,50)
-            << QPoint(10,50)
-            << QPoint(10,30)
-            << QPoint(20,30);
+    poligon << QPoint(0,0)
+            << QPoint(5,6)
+            << QPoint(2,6)
+            << QPoint(2,15)
+            << QPoint(-2,15)
+            << QPoint(-2,6)
+            << QPoint(-5,6);
     return poligon;
 }
 
-void AzGraphicsSelectedItemArrow::show(QPainter *painter, QGraphicsItem *item){
+void AzGraphicsSelectedItemArrow::show(QPainter *painter, QGraphicsItem *){
+   //QPoint point(arrowPos(Npos));
+    QPoint point(arrowPos(SWpos));
+    QPolygon polygone = arrowPolygon();
+    polygone.translate(point);
+    painter->drawPolygon(polygone);
+    //qDebug() <<draw ;
+}
+
+QPoint AzGraphicsSelectedItemArrow::arrowPos(SideLight aPos) const {
+    QList<QGraphicsItem*> items = mScene->selectedItems();
+    if (items.size() == 0)
+        return QPoint();
+    QGraphicsItem *item = items[0];
     QPointF posi = item->pos();
     QRectF bound = item->boundingRect();
-    int x1 = posi.x();
-    int y1 = posi.y();
-    int x2 = x1 + bound.width();
-    int y2 = y1 + bound.height();
-    //painter->drawRect(x1+bound.width()/2,y2,10,20);
-
-    painter->drawPolygon(arrowPolygon(),Qt::WindingFill);
-    //qDebug() << ;
+    QPoint coordinateArrow;
+    QRectF boundArrow = arrowPolygon().boundingRect();
+    switch (aPos) {
+    case Npos:
+        coordinateArrow =  QPoint(posi.x()+bound.center().x(),posi.y()- boundArrow.height());
+        break;
+    case Spos:
+        coordinateArrow =  QPoint(posi.x()+bound.center().x(),posi.y()+bound.height());
+        break;
+    case Epos:
+        coordinateArrow =  QPoint(posi.x()+bound.width(),posi.y()+bound.center().y());
+        break;
+    case Wpos:
+        coordinateArrow =  QPoint(posi.x(),posi.y()+bound.center().y());
+        break;
+    case NEpos:
+        coordinateArrow =  QPoint(posi.x()+bound.width(),posi.y());
+    case NWpos:
+        coordinateArrow =  QPoint(posi.x(),posi.y());
+    case SEpos:
+        coordinateArrow =  QPoint(posi.x()+bound.width(),posi.y()+bound.height());
+    case SWpos:
+        coordinateArrow =  QPoint(posi.x(),posi.y()+bound.height());
+    case NotArrow:
+      break;
+    };
+    return coordinateArrow;
 }
+
--- gui/AzGraphicsSelectedItemArrow.h
+++ gui/AzGraphicsSelectedItemArrow.h
@@ -2,16 +2,30 @@
 #define AZGRAPHICSSELECTEDITEMARROW_H
 #include <QPainter>
 #include <QGraphicsItem>
+#include <QGraphicsScene>
 
 class AzGraphicsSelectedItemArrow
 {
 public:
-    AzGraphicsSelectedItemArrow(){}
+    enum SideLight{
+        Npos = 0,
+        Spos = 1,
+        Epos = 2,
+        Wpos = 3,
+        NEpos = 4,
+        NWpos = 5,
+        SEpos = 6,
+        SWpos = 7,
+        NotArrow = 8
+    };
+   AzGraphicsSelectedItemArrow(QGraphicsScene *);
    ~AzGraphicsSelectedItemArrow(){}
    void show(QPainter* painter, QGraphicsItem* item);
    QPolygon static arrowPolygon();
+   QPoint arrowPos(SideLight) const;
 private:
     AzGraphicsSelectedItemArrow(const AzGraphicsSelectedItemArrow&);
     AzGraphicsSelectedItemArrow& operator = (const AzGraphicsSelectedItemArrow&){}
+    QGraphicsScene *mScene;
 };
 #endif // AZGRAPHICSSELECTEDITEMARROW_H
