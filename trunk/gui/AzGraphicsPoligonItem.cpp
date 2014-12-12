fb869ac3b75b2df2a8d90985789cd7faf5b7e605
nicolay870@gmail.com <nicolay870@gmail.com@e894bab0-515d-e7b2-031d-1052601404cc>
2014-12-12 13:15:54 +0000

    добавлен клас для тестирования полигона

---
+++ trunk/gui/AzGraphicsPoligonItem.cpp
@@ -0,0 +1,32 @@
+#include "azgraphicspoligonitem.h"
+#include <QBrush>
+#include <QDebug>
+
+AzGraphicsPoligonItem::AzGraphicsPoligonItem(QGraphicsItem * parent):QGraphicsPolygonItem(parent) {
+    QPolygon poligon;
+    poligon << QPoint(0,0)
+            << QPoint(27,16)
+            << QPoint(12,16)
+            << QPoint(12,115)
+            << QPoint(-25,115)
+            << QPoint(-25,16)
+            << QPoint(-17,16);
+    setPolygon(poligon);
+    setFlag(QGraphicsItem::ItemSendsGeometryChanges,true);
+    setAcceptHoverEvents(true);
+}
+
+QVariant AzGraphicsPoligonItem::itemChange(GraphicsItemChange change, const QVariant & value) {
+    return QGraphicsPolygonItem::itemChange(change,value);
+}
+
+
+void AzGraphicsPoligonItem::hoverEnterEvent(QGraphicsSceneHoverEvent * event) {
+    setBrush(Qt::yellow);
+    QGraphicsPolygonItem::hoverEnterEvent(event);
+}
+
+void AzGraphicsPoligonItem::hoverLeaveEvent(QGraphicsSceneHoverEvent * event) {
+    setBrush(Qt::white);
+    QGraphicsPolygonItem::hoverLeaveEvent(event);
+}
